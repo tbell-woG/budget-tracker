3d821fe0403536312bd7c59674e92331fa4d2633
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-02-01 20:59:29 -0500

    Added service worker function in HTML

--- server.js
+++ server.js
@@ -12,14 +12,15 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("client"));
+app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useMongoClient: true
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api-routes.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
